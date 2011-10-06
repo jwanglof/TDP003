69db2e496e2dceb9b807c52dd831d64ab5b5dfb4
Johan Wanglof <jwanglof@gmail.com>
2011-10-06 14:35:36 +0200

    Possible to view one technique and see what projects that used that technique. The search-page is implemented but doesn't work yet. 2011-10-06 2:25PM

--- web/flaskProject.py
+++ web/flaskProject.py
@@ -53,5 +53,32 @@ def page_project(id):
     data.init()
     return render_template("project.html", _db_data=data._data, _show_id=int(id))
 
+@app.route("/techniques")
+def page_techs():
+    data.init()
+    _techs = data.retrieve_technique_stats()[1]
+    return render_template("techniques.html", _db_data=data._data, _techniques = _techs)
+
+@app.route("/technique/<tech>")
+def page_tech(tech):
+    data.init()
+    _techs = data.retrieve_technique_stats()[1]
+    return render_template("technique.html", _db_data=data._data, _techniques = _techs,_tech=tech)
+
+@app.route("/search", methods=["GET", "POST"])
+def page_search():
+    data.init()
+    sstring = ""
+    keys = data._data[0].keys()
+
+    if request.method == "POST":
+        _search_string = request.form["search_string"]
+        _search_categories = request.form.getlist("search_categories")
+        if len(_search_categories) > 0:
+            sstring = data.retrieve_projects(search=_search_string, search_fields=_search_categories)[1]
+        else:
+            sstring = "You must specify at least one category to search in!"
+    return render_template("search.html", _db_data=data._data, _search_result = sstring, keys=keys)
+
 if __name__ == "__main__":
     app.run(debug=True)
